7d03e268b9de620b4212eee7e0401b94ce02ea53
laskar-ksatria <dock_fest@yahoo.co.id>
2020-10-02 12:20:18 +0700

    ladd

--- app.js
+++ app.js
@@ -28,10 +28,33 @@ app.use(require('./routes'))
 
 app.use(require('./middlewares/errorHandler'));
 
+const WebSocket = require('ws');
+
+const apiKey = process.env.CRYPTO_COMPARE_API_KEY;
+
+const ccStreamer = new WebSocket('wss://streamer.cryptocompare.com/v2?api_key=' + apiKey);
+
+function walkingWithWebSocket() {
+    ccStreamer.on('open', function open() {
+        var subRequest = {
+            "action": "SubAdd",
+            "subs": ["2~Coinbase~BTC~USD", "2~Coinbase~LTC~USD", "2~Coinbase~ETH~USD", "2~Coinbase~BNB~USD"]
+        };
+        ccStreamer.send(JSON.stringify(subRequest));
+    });
+    ccStreamer.on('message', function incoming(data) {
+        let { PRICE, FROMSYMBOL, TOSYMBOL, VOLUME24HOUR } = JSON.parse(data);
+        if (PRICE && FROMSYMBOL && TOSYMBOL, VOLUME24HOUR) {
+            console.log(PRICE)
+            Io.emit(`realtime-price`, {PRICE, FROMSYMBOL, TOSYMBOL, VOLUME24HOUR});
+        }
+    });
+};
+
+walkingWithWebSocket();
+
 server.listen(PORT, () => console.log(`Server started on ${PORT}`))
 
 Io.on('connection', socket => {
     socket.on(`disconnect`, () => {})
 });
-
-require('./helpers/websocket.config')(Io);
