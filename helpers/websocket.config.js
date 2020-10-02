c32686c5fa5dc44e63b27183019122c126a93a4b
laskar-ksatria <dock_fest@yahoo.co.id>
2020-10-02 10:58:41 +0700

    addin

--- helpers/websocket.config.js
+++ helpers/websocket.config.js
@@ -12,11 +12,10 @@ function walkingWithWebSocket(Io) {
         };
         ccStreamer.send(JSON.stringify(subRequest));
     });
-
     ccStreamer.on('message', function incoming(data) {
-        // console.log(JSON.parse(data));
         let { PRICE, FROMSYMBOL, TOSYMBOL, VOLUME24HOUR } = JSON.parse(data);
         if (PRICE && FROMSYMBOL && TOSYMBOL, VOLUME24HOUR) {
+            console.log(PRICE)
             Io.emit(`realtime-price`, {PRICE, FROMSYMBOL, TOSYMBOL, VOLUME24HOUR});
         }
     });
